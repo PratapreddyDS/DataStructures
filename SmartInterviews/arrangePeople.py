327641cd1450fad912a12cee9a5b1dbfef8d297d
Pratap Reddy <pratapreddyds@gmail.com>
2024-11-04 09:59:01 -0600

    Solved the arrangePeopleSeating problem

--- SmartInterviews/arrangePeople.py
+++ SmartInterviews/arrangePeople.py
@@ -1,45 +1,81 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 import math
 
-# N = int(input())
-# M = int(input())
-# ar = list(map(int,input().split()))
-
-N = 4
-M = 7
-ar = [0,0,1,0,0,0,0]
+N = int(input())
+M = int(input())
+ar = list(map(int,input().split()))
 
 
-cnt = 0
+people = 0
+vacant = 0
+
+last = -1
+
+if len(ar) == 1 and ar[0] == 0:
+    people = 1
+
+for i in range(M):
+    if ar[i] == 0:
+        vacant += 1
+    else:
+        if vacant > 1 and last == -1:
+            people += (vacant//2)
+        elif vacant > 2 and (last+vacant) <= M-1:
+            people += (vacant-1)//2
+
+        vacant = 0
+        last = i
+
+if vacant > 1 and last != -1:
+    people += (vacant//2)
+
+if vacant > 1 and last == -1:
+    people += (vacant+1)//2
 
 
-if sum(ar) == 0:
-    cnt += math.ceil(ar/2)
 
 
+if people >= N:
+    print("YES")
 else:
+    print("NO")
 
 
-    indexArray = []
-    for i in range(M):
-        if ar[i] == 1:
-            indexArray.append(i)
 
 
-    last = -1
-    for j in range(len(indexArray)):
-        cnt += ((indexArray[j]-last)-1)//2
-        last = indexArray[j]
-        print(cnt)
-
-    if  M - indexArray[-1] > 1:
-        cnt += (M - indexArray[-1])//2
-        print(cnt, (M - indexArray[-1])//2)
-
-    cnt += len(indexArray)
 
 
-# print(indexArray,cnt)
-if N == cnt:
-    print('YES')
-else:
-    print('NO')
+
+
+
+
+
+
+
+
+# cnt = 0
+
+# indexArray = []
+# for i in range(M):
+#     if ar[i] == 1:
+#         indexArray.append(i)
+
+# last = -1
+# prev = 0
+# for j in range(len(indexArray)):
+#     if (indexArray[j] - (prev+1)) > 2:
+#         cnt += abs(((indexArray[j]-last)-1)//2)
+#     last = indexArray[j]
+#     prev = indexArray[j]
+#     # print(cnt)
+
+# if len(indexArray) == 0:
+#     cnt += math.ceil(len(ar)/2)
+
+
+
+# # print(indexArray,cnt)
+# if N == cnt:
+#     print('YES')
+# else:
+#     print('NO')
